b632b7ff3a5da27629007f41fac46d6f69ba458e
IceCandy7 <145496072+IceCandy7@users.noreply.github.com>
2025-02-07 16:53:12 +0800

    TA1 .py

--- TA1.py
+++ TA1.py
@@ -1,6 +1,4 @@
-# Auto detect text files and perform LF normalization
-# * text=auto
-# 1. Character Counter
+
 def count_characters(input_string):
     vowels = 0
     consonants = 0
@@ -22,8 +20,6 @@ def count_characters(input_string):
     print("Spaces:", spaces)
     print("Other Characters:", others)
 
-
-# 2. Digit Sum
 def sum_digits(input_string):
     digit_sum = 0
     for char in input_string:
@@ -32,38 +28,31 @@ def sum_digits(input_string):
 
     print("Sum of Digits:", digit_sum)
 
-
-# 3a. Nested For Loop Pattern
 def pattern_for():
     for i in range(1, 6):
         for j in range(1, i + 1):
             print(j, end="")
         print()
 
-
-# 3b. Nested While Loop Pattern
 def pattern_while():
     i = 1
     num = 1
     while i <= 5:
         j = 1
         while j <= i:
-            if i % 2 != 0:  # Check if i is odd for the first pattern
+            if i % 2 != 0:  
               print(num, end="")
-            else: # For the second pattern
+            else: 
               print(i+2, end="")
             j += 1
-        if i % 2 != 0: # Increment for the first pattern
+        if i % 2 != 0: 
           num += 2
         print()
         i += 1
 
 
-
-# Get input from the user (you can modify these for testing)
 input_string = input("Enter a string: ")
 
-# Call the functions
 count_characters(input_string)
 sum_digits(input_string)
 
